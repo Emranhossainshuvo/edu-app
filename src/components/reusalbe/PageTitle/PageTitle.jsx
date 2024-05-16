8fbbd79a89b569193d6e5720b90899e3465a362f
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-16 21:24:20 +0600

    added some changes in the page title file

--- src/components/reusalbe/PageTitle/PageTitle.jsx
+++ src/components/reusalbe/PageTitle/PageTitle.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 const PageTitle = ({text}) => {
     return (
         <>
+
             <p className="my-5 text-3xl text-[#06286E]">{text}</p>
+            
         </>
     );
 };
